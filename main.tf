20942e5f12f4237af384c01e2135662f3b540045
wiseelf <wiseelf@gmail.com>
2024-09-03 09:34:48 +0200

    fix: Remvoves duplicates

--- main.tf
+++ main.tf
@@ -618,15 +618,15 @@ locals {
 
   subgroup_label_map = { for group_name, label in gitlab_group_label.subgroup_labels : label.name => label.label_id }
 
-  # Extract `share_groups` configurations from each group
-  share_groups = flatten([
+  # Extract `share_groups` configurations from each group and remove duplicates
+  share_groups = distinct(flatten([
     for group in var.gitlab_groups : [
       for share in lookup(group, "settings", {})["share_groups"] : merge(
         share,
         { group_id = group.name }
       )
     ] if contains(keys(lookup(group, "settings", {})), "share_groups")
-  ])
+  ]))
 
   # Map for parent group namespaces
   parent_group_id_map = { for group in local.parent_groups : group.name => gitlab_group.parent_groups[group.name].id if length(try(gitlab_group.parent_groups[group.name].id, "")) > 0 }
@@ -1535,7 +1535,7 @@ locals {
 # Lookup the group id to be shared
 data "gitlab_group" "group_id" {
   for_each = {
-    for group in local.share_groups : group["group_id"] => group...
+    for group in local.share_groups : group["group_id"] => group
   }
 
   full_path = each.key
@@ -1546,7 +1546,7 @@ data "gitlab_group" "group_id" {
 # Lookup the share group id (the group to share with)
 data "gitlab_group" "share_group_id" {
   for_each = {
-    for group in local.share_groups : group["share_group_id"] => group...
+    for group in local.share_groups : group["share_group_id"] => group
   }
 
   full_path = each.key
